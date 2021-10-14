e397818438bbb5cb3e393d59f6ac15b3c8f559a2
buixuantan <123xuantan@gmail.com>
2021-10-14 17:01:32 +0700

    ok

---
+++ System-time/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args){
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.print("i = " + i);
+        System.out.print("f = " + f);
+        System.out.print("d = " + d);
+        System.out.print("b = " + b);
+        System.out.print("c = " +c);
+        System.out.print("s = " + s);
+    }
+}
---
+++ System-time/src/codegym/vn/Main.java
@@ -0,0 +1,19 @@
+package codegym.vn;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the width: ");
+        double width = scanner.nextDouble();
+        System.out.println("Enter the height: ");
+        double height = scanner.nextDouble();
+
+        Rectangle rectangle = new Rectangle(width, height);
+        System.out.println("Your " + rectangle.display());
+        System.out.println("Rectangle Area: \n" + rectangle.getArea());
+        System.out.println("Rectangle Perimeter: \n" + rectangle.getPerimeter());
+
+    }
+}
